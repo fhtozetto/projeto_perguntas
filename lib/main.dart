10ba4d7623b9f3f0c88bd9bb1ad21fe0c351608d
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2025-12-24 16:31:04 -0300

    Usando Column para Organizar os Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,25 @@ class PerguntaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+      'O Corinthians foi campeão da copa do Brasil em 2025?',
+      'Qual o seu instrumento musical favorito?',
+    ];
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // Retira o banner de debug
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Perguntas')
+        appBar: AppBar(title: const Text('Perguntas')),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            ElevatedButton(onPressed: () {}, child: const Text('Resposta 1')),
+            ElevatedButton(onPressed: () {}, child: const Text('Resposta 2')),
+            ElevatedButton(onPressed: () {}, child: const Text('Resposta 3')),
+          ],
         ),
-        body: Text('Olá Flutter!!!'),
       ),
     );
   }
