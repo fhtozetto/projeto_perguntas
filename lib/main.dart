4cafd86cc08c6ab8d88a9b9a9b460069ac176a8d
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2025-12-24 15:30:57 -0300

    Iniciando Projeto

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Text('Olá Flutter'));
+  }
+}
